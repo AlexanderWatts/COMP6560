62355667df0746b939aece993f84cb8c0bfec809
Alexander Watts <acwatts.dev@gmail.com>
2021-10-07 13:12:25 +0100

    Removed unused import

--- GA.java
+++ GA.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -60,7 +59,7 @@ public class GA
             //----------------------------------------------------------//
             
             // TODO
-            
+
             //----------------------------------------------------------//
             // evaluates the new population                             //
             //----------------------------------------------------------//
