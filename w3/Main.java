6e5114d2c48dec9a4959636064341259874a19d3
Alexander Watts <acwatts.dev@gmail.com>
2021-10-20 23:12:26 +0100

    Added more parameters including crossover method, selection method and random number generator

--- w3/Main.java
+++ w3/Main.java
@@ -1,9 +1,12 @@
 import org.epochx.gp.model.EvenParity;
 import org.epochx.gp.model.GPModel;
+import org.epochx.gp.op.crossover.UniformPointCrossover;
 import org.epochx.life.GenerationAdapter;
 import org.epochx.life.Life;
+import org.epochx.op.selection.TournamentSelector;
 import org.epochx.stats.StatField;
 import org.epochx.stats.Stats;
+import org.epochx.tools.random.MersenneTwisterFast;
 
 public class Main {
   public static void main(String[] args) {
@@ -16,6 +19,10 @@ public class Main {
     model.setPopulationSize(500);
     model.setNoGenerations(100);
     model.setMaxDepth(8);
+
+    model.setCrossover(new UniformPointCrossover(model));
+    model.setProgramSelector(new TournamentSelector(model, 7));
+    model.setRNG(new MersenneTwisterFast());
 
     Life.get().addGenerationListener(new GenerationAdapter() {
       public void onGenerationEnd() {
