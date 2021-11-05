e95aad6d545284ceb02376c790d086863c5ab626
Alexander Watts <acwatts.dev@gmail.com>
2021-11-05 17:48:43 +0000

    Created and added initial files

---
+++ w5/Main.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    Trader t1 = new Trader();
+    t1.load("Unilever.csv"); 
+  }
+}
