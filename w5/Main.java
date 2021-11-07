3ac2b40174d45a32cf6ea008f536ac1822ecf550
Alexander Watts <acwatts.dev@gmail.com>
2021-11-07 12:50:39 +0000

    Created main class and instance of trader to run program

--- w5/Main.java
+++ w5/Main.java
@@ -4,5 +4,7 @@ public class Main {
   public static void main(String[] args) throws IOException {
     Trader t1 = new Trader();
     t1.load("Unilever.csv"); 
+
+    System.out.println(t1.trade());
   }
 }
